ac8a730e3f16d6a57c9ea01c3e0e79be0f44ff7f
carlos <ccarbonell@gmail.com>
2025-04-01 20:50:02 +0200

    versión con stats e imágenes y pausas entre scroll

--- source/helpers.py
+++ source/helpers.py
@@ -1,6 +1,14 @@
 import configparser
 import os
+import requests
+from furl import furl
+import re
 
+"""
+Funciones de ayuda para scrapping y procesamiento básico de contenidos
+"""
+
+# Creamos un archivo de configuración con usuario y contraseña
 def create_config(config_file_path, user, pwd):
     config = configparser.ConfigParser()
     config.add_section('Credenciales')
@@ -9,19 +17,76 @@ def create_config(config_file_path, user, pwd):
     with open(config_file_path, 'w') as config_file:
         config.write(config_file)
 
+# Obtenemos usuario y contraseña desde el archivo settings.ini o los pedimos al usuario si no existe
 def get_config():
     config_file_path = 'settings.ini'
     config = configparser.ConfigParser()
+    
+    # Comprobamos si el archivo ya existe
     if os.path.isfile(config_file_path):
+        # Leemos la configuración existente
         config.read(config_file_path)
         user = config.get('Credenciales', 'usuario')
         pwd = config.get('Credenciales', 'contraseña')
         print('Configuración cargada: Usuario - {}'.format(user))
     else:
-        print("settings.ini no existe, introduce tu usuario y contraseña de X.com y los datos se guardarán para la próxima vez")
+        # Pedimos las credenciales al usuario y guardarlas
+        print("settings.ini no existe, introduce tu usuario y contraseña de X.com")
         user = input('Introduce el usuario: ')
         pwd = input('Introduce la contraseña: ')
         create_config(config_file_path, user, pwd)
         print('Configuración guardada en el archivo settings.ini.')
     
     return user, pwd
+
+# Descargamos archivo multimedia usando las cookies del navegador
+def downloadmedia(url, driver, downloadfolder):
+    # Creamos carpeta de destino (si no existe)
+    destfolder = downloadfolder
+    os.makedirs(destfolder, exist_ok=True)
+
+    # Extraemos cookies del navegador 
+    session = requests.Session()
+    for cookie in driver.get_cookies():
+        session.cookies.set(cookie["name"], cookie["value"])
+
+    # Hacemos la petición usando la sesión con cookies
+    response = session.get(url)
+    
+    if response.status_code == 200:
+        # Determinamos el nombre del archivo a partir de la URL
+        f = furl(url)
+        file_name = str(f.path.segments[-1]) + "." + f.args["format"]
+        
+        # Solicitamos la versión más grande de la imagen
+        f.set({"format": "jpg", "name": "large"})
+        url = f.url
+        
+        # Guardamos el archivo en disco
+        full_path = os.path.join(destfolder, file_name)
+        with open(full_path, "wb") as f:
+            f.write(response.content)
+        
+        print("Descargado en", full_path)
+        return file_name
+    else:
+        print("Error:", response.status_code)
+        return False
+
+def processstats(stat):
+    v = str(stat.text)
+
+    # Convertimos a int aunque exista texto no numérico
+    digitos = re.findall(r'\d+', v)
+    if digitos:
+        vp = int(''.join(digitos))
+    else:
+        vp = 0 
+
+    # Multiplicamos si existen las letras K = 1000 o M = 1000000
+    if "K" in v:
+        vp*=1000
+    if "M" in v:
+        vp*=1000000
+
+    return vp
--- source/x.py
+++ source/x.py
@@ -7,73 +7,135 @@ from time import sleep
 from helpers import *
 import sys
 
-
 """
 Scrapper para X.com
-
 """
-# Variables: TODO: integrar con input()
-termino = 'Agresión'
+
+# Definimos las variables de búsqueda
+termino = 'Winsconsin'
 ubicacion = 'Spain'
 
+# Obtenemos las credenciales para x.com
+user, pwd = get_config()
 
-#obtenemos las credenciales para x.com
-user,pwd=get_config()
-
+# Iniciamos el navegador
 browser = webdriver.Firefox()
 browser.maximize_window()
 wait = WebDriverWait(browser, 30)
 browser.get('https://x.com/login')
 
-
-# realizamos el login
+# Hacemos  login
 username_input = wait.until(EC.visibility_of_element_located((By.NAME, "text")))
 username_input.send_keys(user)
 
-
-nextbtn=WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Next')]")))
+# Pulsamos el botón "Next"
+nextbtn = wait.until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Next')]")))
 nextbtn.click()
-
 sleep(3)
 
-#password
-username_input = wait.until(EC.visibility_of_element_located((By.NAME, "password")))
-username_input.send_keys(pwd)
-username_input.send_keys(Keys.ENTER)
-
+# Introducir contraseña y hacer login
+password_input = wait.until(EC.visibility_of_element_located((By.NAME, "password")))
+password_input.send_keys(pwd)
+password_input.send_keys(Keys.ENTER)
 sleep(3)
 
-# Realizar la búsqueda
-
-query = f'{termino} place:{ubicacion}'
-
+# Realizar la búsqueda por parámetros
+query = f'{termino} place:{ubicacion} since:2006-01-01 min_faves:5 min_retweets:0 '
 url = f'https://x.com/search?q={query}'
 
 browser.get(url)
-wait = WebDriverWait(browser, 30)
 
+# Creamos una lista para guardar los datos extraídos
+data = []
 
+# Esperamos a que aparezcan los primeros posts
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-testid="cellInnerDiv"]')))
+post_counter = 0
+intentos_sin_nuevos_posts = 0
+max_intentos = 5
 
-# Empezamos el scrapping
+# Empezamos el scraping
 while True:
+    # Buscamos posts
+    posts = browser.find_elements(By.CSS_SELECTOR, 'div[data-testid="cellInnerDiv"]')
 
-    posts=browser.find_elements(By.CSS_SELECTOR,'div[data-testid="cellInnerDiv"]')
+    # Procesamos los nuevos posts
+    nuevos_posts = 0
+    for p in posts[post_counter:]:
+        try:
+            # Obtenemos la hora del post
+            timeelem = p.find_element(By.CSS_SELECTOR, 'time[datetime]')
+            parent = timeelem.find_element(By.XPATH, '..')
 
-    for p in posts:
-        #try:
-            timeelem=p.find_element(By.CSS_SELECTOR,'time[datetime]')
-            parent=timeelem.find_element(By.XPATH, '..')
-            post={
-                'time':timeelem.get_attribute("datetime"),
-                'posturl':parent.get_attribute("href"),
-                'username':p.find_element(By.CSS_SELECTOR,'.css-1jxf684.r-dnmrzs.r-1udh08x.r-1udbk01.r-3s2u2q.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3').text
+            # Inicializamos lista de elementos multimedia
+            media = []
+
+            # Buscamos imágenes asociadas
+            img_elements = p.find_elements(By.TAG_NAME, "img")
+            for img in img_elements:
+                imgsrc = img.get_attribute("src")
+                if "/media/" in imgsrc:
+                    try:
+                        # Utilizamos una función de helpers
+                        filename = downloadmedia(imgsrc, browser, '../media/')
+                        if filename:
+                            media.append(filename)
+                    except Exception as e:
+                        print("No se pudo descargar la imagen:", e)
+
+            # Buscamos los elementos numéricos del post
+            statsdom = p.find_elements(By.CSS_SELECTOR, ".css-175oi2r.r-18u37iz.r-1h0z5md.r-13awgt0")
+
+            stats={
+                'replies':processstats(statsdom[0]),
+                'reposts':processstats(statsdom[1]),
+                'likes':processstats(statsdom[2]),
+                'visitas':processstats(statsdom[3])
             }
-            print(post)
-            sys.exit()
-        #except Exception as e:
-        #    print("Hubo un error al procesar el post:",e)
 
-            
-            
-            
-            
+
+            # Creamos un diccionario para el post
+            post = {
+                'time': timeelem.get_attribute("datetime"),
+                'posturl': parent.get_attribute("href"),
+                'username': p.find_element(By.CSS_SELECTOR, '.css-1jxf684.r-dnmrzs.r-1udh08x.r-1udbk01.r-3s2u2q.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3').text,
+                'text': p.find_element(By.CSS_SELECTOR, '.css-146c3p1.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-1udbk01.r-bcqeeo.r-1ttztb7.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim').get_attribute('innerHTML'),
+                'media': media,
+                'stats': stats
+            }
+
+            if post not in data:
+                data.append(post)
+                nuevos_posts += 1
+                #print(post)
+                print(post["time"],post["username"],"::::")
+
+        except Exception as e:
+            # Si no se puede procesar el post mostramos el error y su origen
+            print("Hubo un error al procesar el post:", e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+    post_counter = len(posts)
+
+    # Comprobamos si hay nuevos posts
+    if nuevos_posts == 0:
+        intentos_sin_nuevos_posts += 1
+        print(f"No se detectaron nuevos posts ({intentos_sin_nuevos_posts}/{max_intentos})")
+        if intentos_sin_nuevos_posts >= max_intentos:
+            print("No se encontraron más posts. Finalizando scraping.")
+            print(f"Se han procesado {len(data)} posts.")
+            break
+    else:
+        intentos_sin_nuevos_posts = 0
+
+    # Hacemos scroll para que aparezcan nuevos posts
+    # Intentamos ralentizar el proceso para no limitar el ratio de posts recibidos
+    print("Haciendo scroll...")
+    for i in range(5):
+        browser.execute_script("window.scrollBy(0, 500);")
+        sleep(0.8)  
+
+    # Pausa
+    sleep(3)
